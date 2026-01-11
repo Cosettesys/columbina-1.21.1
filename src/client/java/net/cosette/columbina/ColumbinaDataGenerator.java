ef36b3062baf5af3d4ce5c7684068c67e424eea7
Cosette <cosettesys@gmail.com>
2026-01-11 17:32:28 +0100

    first push

---
+++ src/client/java/net/cosette/columbina/ColumbinaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cosette.columbina;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ColumbinaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
